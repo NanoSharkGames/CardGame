70937bc82d2d5ade12779c847098accc0df0df13
Nicholas Trevino <nicholasltrev@gmail.com>
2020-11-06 20:10:55 -0600

    State machine base complete

---
+++ SRC/Assets/Scripts/StateMachine/CardGame/EnemyTurnCardGameState.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class EnemyTurnCardGameState : CardGameState
+{
+    public static event Action EnemyTurnBegan;
+    public static event Action EnemyTurnEnded;
+
+    [SerializeField] float _pauseDuration = 1.5f;
+
+    public override void Enter()
+    {
+        Debug.Log("Enemy Turn: ...Entering");
+        EnemyTurnBegan?.Invoke();
+
+        StartCoroutine(EnemyThinkingRoutine(_pauseDuration));
+    }
+
+    public override void Exit()
+    {
+        Debug.Log("Enemy Turn: Exiting...");
+    }
+
+    IEnumerator EnemyThinkingRoutine(float pauseDuration)
+    {
+        Debug.Log("Enemy thinking...");
+        yield return new WaitForSeconds(pauseDuration);
+
+        Debug.Log("Enemy performs action");
+        EnemyTurnEnded?.Invoke();
+        // Turn over. Go back to player
+        StateMachine.ChangeState<PlayerTurnCardGameState>();
+    }
+}
---
+++ SRC/Assets/Scripts/StateMachine/CardGame/PlayerTurnCardGameState.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerTurnCardGameState : CardGameState
+{
+
+    [SerializeField] Text _playerTurnTextUI = null;
+
+    int _playerTurnCount = 0;
+
+    public override void Enter()
+    {
+        Debug.Log("Player Turn: ...Entering");
+        _playerTurnTextUI.gameObject.SetActive(true);
+
+        _playerTurnCount++;
+        _playerTurnTextUI.text = "Player Turn " + _playerTurnCount.ToString();
+
+        // Hook into events
+        StateMachine.Input.PressedConfirm += OnPressedConfirm;
+    }
+
+    public override void Exit()
+    {
+        _playerTurnTextUI.gameObject.SetActive(false);
+        Debug.Log("Player Turn: Exiting...");
+
+        // Unhook from events
+        StateMachine.Input.PressedConfirm -= OnPressedConfirm;
+    }
+
+    void OnPressedConfirm()
+    {
+        StateMachine.ChangeState<EnemyTurnCardGameState>();
+    }
+}
